a90ec0c18eb9a5c357afd1c394a62b8b1faa9d03
jordanoboyle <jmcmcode@gmail.com>
2024-07-01 15:28:40 -0600

    useEffect handleQuotesIndex in content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { QuotesIndex } from "./QuotesIndex"
 
 
@@ -7,22 +7,22 @@ import { QuotesIndex } from "./QuotesIndex"
 export function Content() {
   const [quoteData, setQuoteData] = useState([])
 
-  const handleQuoteData = () => {
+  const handleQuotesIndex = () => {
     console.log("getting the data")
     axios.get("https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json").then(response => {
-      console.log(response.data)
-      setQuoteData(response.data)
+      console.log(response.data);
+      setQuoteData(response.data);
       
     })
-    
   }
+  
+  useEffect(handleQuotesIndex, []);
 
   return (
     <main>
       <h1>Welcome to React!</h1>
-      <button onClick={handleQuoteData}>get the data</button>
+      {/* <button onClick={handleQuotesIndex}>push</button> */}
       <QuotesIndex quotes={quoteData}/>
-    
     </main>
   )
 }
--- src/QuotesIndex.jsx
+++ src/QuotesIndex.jsx
@@ -2,11 +2,19 @@
 
 export function QuotesIndex(props) {
   
+
   
 
   return(
     <div>
       <h1>This is from QuotesIndex</h1>
+      <div>
+      {props.quotes.map(quoteData => (
+        <div>
+          <p>Source: {quoteData.source}</p>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
