9d1f3a9f7e2e569604c95679146fef5aa494d14a
jordanoboyle <jmcmcode@gmail.com>
2024-07-01 14:53:35 -0600

    initial data retrieval via axios

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,24 @@
+import axios from "axios"
+import { useState } from "react"
+
 export function Content() {
+  const [quoteData, setQuoteData] = useState([])
+
+  const handleQuoteData = () => {
+    console.log("getting the data")
+    axios.get("https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json").then(response => {
+      console.log(response.data)
+      setQuoteData(response.data)
+      
+    })
+    
+  }
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <button onClick={handleQuoteData}>get the data</button>
+    {/* <p>{quoteData}</p> */}
     </main>
   )
 }
