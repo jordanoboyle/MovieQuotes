097ed7e325d63180e27e1b7f134b5c6163679ee5
jordanoboyle <jmcmcode@gmail.com>
2024-07-01 18:19:53 -0600

    fix key=id issue, adjust to function(element, index)

--- src/Content.jsx
+++ src/Content.jsx
@@ -21,7 +21,6 @@ export function Content() {
   return (
     <main>
       <h1>Welcome to React!</h1>
-      {/* <button onClick={handleQuotesIndex}>push</button> */}
       <QuotesIndex quotes={quoteData}/>
     </main>
   )
--- src/QuotesIndex.jsx
+++ src/QuotesIndex.jsx
@@ -3,17 +3,17 @@
 export function QuotesIndex(props) {
   
 
-  
+
 
   return(
     <div>
       <h1>This is from QuotesIndex</h1>
       <div>
-      {props.quotes.map(quoteData => (
-        <div>
+      {props.quotes.map(function(quoteData, index) {
+        <div key={index}>
           <p>Source: {quoteData.source}</p>
         </div>
-      ))}
+      })}
       </div>
     </div>
   )
