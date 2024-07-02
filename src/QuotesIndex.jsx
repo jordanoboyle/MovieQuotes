cea74c0fadd1877d4adf5f60fe1c802a0dd3044d
jordanoboyle <jmcmcode@gmail.com>
2024-07-01 19:16:03 -0600

    index css grid to QuotesIndex, presentable

--- src/QuotesIndex.jsx
+++ src/QuotesIndex.jsx
@@ -1,19 +1,22 @@
+import {useState, useEffect} from "react"
+
 
 
 export function QuotesIndex(props) {
-  
-
 
 
   return(
     <div>
       <h1>This is from QuotesIndex</h1>
-      <div>
-      {props.quotes.map(function(quoteData, index) {
-        <div key={index}>
+      <div className="QuoteCardGrid">
+      {props.quotes.map((quoteData, index) =>(
+        <div key={index} className="QuoteCardGridItem">
+          <p>Category: {quoteData.theme}</p>
+          <p>Title: {quoteData.context}</p>
           <p>Source: {quoteData.source}</p>
+          <p>Quote: {quoteData.quote}</p>
         </div>
-      })}
+      ))}
       </div>
     </div>
   )
